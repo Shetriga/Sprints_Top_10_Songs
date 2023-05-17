53d5bd1973c418c87d1ba902b4edb2d2acb819d7
Mahmoud Atef Abouelsoud Ali <mahmoud.atef98@outlook.com>
2023-05-17 20:10:28 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById("showMe").addEventListener("click", (event) => {
+    // console.log('I am Clicked!!');
+    document.getElementById("showMe").style.display = "none";
+    document.getElementById("flexDiv").style.display = 'flex';
+});
+// document.getElementById("showMe").style.visibility = "hidden";
+document.getElementById("flexDiv").style.display = 'none';
